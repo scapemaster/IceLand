ab14ec041ee01091256f93d4d6b13f2d33fea92a
scapemaster <andreas.lanzl@gmx.de>
2023-09-30 10:11:20 +0200

    Update config.php

--- minimarkt_iceland_2.0_wc/config.php
+++ minimarkt_iceland_2.0_wc/config.php
@@ -5,14 +5,14 @@ Das hier ist das IceLand Framework Version 2.0.
 // Datenbank Verbindung
 $db_host = 'localhost';
 $db_user = 'mima';
-$db_pwd  = 'Yttrium02!';
+$db_pwd  = '*********';
 $db_name = 'mima';
 
 $db_con  = mysqli_connect($db_host, $db_user, $db_pwd, $db_name);
 
 
 // Website Stamm-URL
-$rooturl = "http://gmork/minimarkt/htdocs/";
+$rooturl = "http://minimarkt/htdocs/";
 
 // App Name
 $app_name = "minimarkt";
